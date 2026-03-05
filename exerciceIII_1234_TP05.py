8fe8ff8ef0ac6ce657a4ba71753a7da8fc4a0931
Laurent Keletaona <keletaona.laurent@gmail.com>
2026-03-05 15:01:27 +1100

    TP 5 terminé

---
+++ exerciceIII_1234_TP05.py
@@ -0,0 +1,18 @@
+# print("On fais ça bien")
+# print("Bon comme ça")
+
+#nom = input ("Qui es-tu ? ")
+#print("Bonjour, je suis", nom)
+
+#a = int(input("1er nombre : "))
+#b = int(input("2ème nombre : "))
+# addition = a + b
+# print("La somme est :", addition)
+
+#x = float(input("Nombre 1 : "))
+#y = float(input("Nombre 2 : "))
+#print ("La moyenne est :", (x+y) / 2)
+
+#euros = float(input("Montant en euros : "))
+#print("En centimes :", euros * 100)
+
